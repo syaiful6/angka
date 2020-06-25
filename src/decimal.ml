8f6f9641eb3f4324b85db4b9e7ac9c1e11fa17b0
syaiful6 <syai.bahri6@gmail.com>
2020-06-25 23:24:35 +0700

    rename to angka

---
+++ src/decimal.ml
@@ -0,0 +1,130 @@
+module Zutils = struct
+  let ten = Z.of_int 10
+
+  let mul_exp10 i n =
+    Z.mul i (Z.pow ten n)
+
+  let is_zero x = Z.equal x Z.zero
+
+  let cdiv_exp10 i n =
+    Z.div i (Z.pow ten n)
+
+  let cdiv_mod_exp10 i n =
+    if n <= 0 then (i, 0) else begin
+      let cq = cdiv_exp10 i n in
+      let cr = Z.sub i (mul_exp10 cq n) in
+      (cq, Z.to_int cr)
+    end
+
+  let div_mod_exp10 i n =
+    let (cq, cr) = cdiv_mod_exp10 i n in
+    if cr > 0 then (cq, cr) else (Z.pred cq, cr - 1)
+
+  let rec is_exp10_rec a j =
+    if is_zero a then j
+    else begin
+      let (quotien, remainder) = Z.div_rem a ten in
+      if is_zero remainder then is_exp10_rec quotien (j + 1)
+      else j
+    end
+
+  let is_exp10 x = is_exp10_rec x 0
+   
+end
+
+module Iutils = struct
+  let is_even n =  n mod 2 = 0
+
+  let pow base exponent =
+    if exponent < 0 then invalid_arg "exponent can not be negative" else
+    let rec aux accumulator base = function
+      | 0 -> accumulator
+      | 1 -> base * accumulator
+      | e when is_even e -> aux accumulator (base * base) (e / 2)
+      | e -> aux (base * accumulator) (base * base) ((e - 1) / 2) in
+    aux 1 base exponent
+
+  let mul_exp10 i n = i * (pow 10 n)
+
+  let exp10 = mul_exp10 1
+end
+
+type t =
+  { num : Z.t
+  ; exp : int
+  }
+
+let create num exp =
+  { num = num
+  ; exp = exp
+  }
+
+let zero = create (Z.of_int 0) 0
+
+let round_exp exp =
+  if exp = 0 then exp else 7 * (exp / 7)
+
+let of_zarith i exp =
+  let x = round_exp exp in
+  let diff = exp - x in
+  if diff = 0 then create i exp else create (Zutils.mul_exp10 i diff) x
+
+let reduce x =
+  let p = Zutils.is_exp10 x.num in
+  if p <= 0 then x else begin
+    let exp = x.exp + p in
+    let rexp = round_exp exp in
+    if rexp = x.exp then x else of_zarith (Zutils.cdiv_exp10 x.num p) exp
+  end
+
+let expand x e =
+  if x.exp <= e then x else of_zarith (Zutils.mul_exp10 x.num (x.exp - e)) e
+
+let add x y =
+  let e = min x.exp y.exp in
+  let xx = expand x e in
+  let yy = expand y e in
+  of_zarith (Z.add xx.num yy.num) e
+
+type round =
+    HalfEven
+  | HalfCeil
+  | HalfFloor
+  | HalfTruncate
+  | HalfAwayFromZero
+  | Ceil
+  | Floor
+  | Truncate
+  | AwayFromZero
+
+let round_to_prec x ?prec:(prec=0) ?round:(round=HalfEven) =
+  if x.exp >= Int.neg prec then x
+  else begin
+    let cx = reduce x in
+    let p = (Int.neg x.exp) - prec in
+    if p < 0 then cx else begin
+      let (q, r) = Zutils.div_mod_exp10 cx.num p in
+
+      let round_half keep =
+        let half = Iutils.exp10 p / 2 in
+        if r = half then
+          if keep then q else Z.succ q
+        else if r > half then Z.succ q
+        else q
+      in
+
+      let q1 = if r = 0 then q else match round with
+          HalfEven  -> round_half (Z.is_even q)
+        | HalfFloor -> round_half true
+        | HalfCeil  -> round_half false
+        | HalfTruncate -> round_half (Z.sign q > 0)
+        | HalfAwayFromZero -> round_half (Z.sign q < 0)
+        | Floor -> q
+        | Ceil  -> Z.succ q
+        | Truncate -> if Z.sign q > 0 then q else Z.succ q
+        | AwayFromZero -> if Z.sign q < 0 then q else Z.succ q
+      in
+
+      of_zarith q1 (Int.neg prec)
+    end
+  end
